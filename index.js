8af630657012e3a47650b098cd5ad968c7336def
Mohammed Ameen <jegroamin@gmail.com>
2018-08-20 09:48:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-function justInvoke(fn){
-  let returnValue = fn();
-  return returnValue;
+function justInvoke(fn) {
+  return fn();
 }
 
-function setThisWithCall(fn, thisValue, arg){
-  return fn.call(thisValue,arg);
+function setThisWithCall(fn, thisValue, argument) {
+  return fn.call(thisValue, argument);
 }
 
-function setThisWithApply(fn, thisValue, args){
-  return fn.apply(thisValue,args);
+function setThisWithApply(fn, thisValue, ourArguments) {
+  return fn.apply(thisValue, ourArguments);
 }
 
-function returnNewFunctionOf(functionToBeCopied, thisValue){
-  return functionToBeCopied.bind(thisValue);
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  const copy = functionToBeCopied.bind(thisValue);
+  return copy;
 }
+// function justInvoke(fn){
+//   let returnValue = fn();
+//   return returnValue;
+// }
+
+// function setThisWithCall(fn, thisValue, arg){
+//   return fn.call(thisValue,arg);
+// }
+
+// function setThisWithApply(fn, thisValue, args){
+//   return fn.apply(thisValue,args);
+// }
+
+// function returnNewFunctionOf(functionToBeCopied, thisValue){
+//   return functionToBeCopied.bind(thisValue);
+// }
